06bd7cfda4be0a866024fb8521d307cd7038d8ce
baczu <kbaczu@gmail.com>
2018-05-28 22:33:28 +0200

    invite

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,7 +1,12 @@
 # Kontroler zapraszajacy uzytkownikow do systemu
 class InvitationsController < Devise::InvitationsController
+  before_filter :check_admin, except: [:edit, :update]
 
   protected
+
+  def check_admin
+    redirect home_path unless current_user.admin?
+  end
 
   def invite_resource
     resource_class.invite!(invite_params, current_inviter) do |u|
